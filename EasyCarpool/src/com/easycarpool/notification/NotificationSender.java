ce7acb5a4d27513c436d8ec7379202591c73149f
ranjit <ranjitjitu@gmail.com>
2016-05-27 00:02:48 +0530

    device registration update

--- EasyCarpool/src/com/easycarpool/notification/NotificationSender.java
+++ EasyCarpool/src/com/easycarpool/notification/NotificationSender.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.apache.log4j.Level;
 
 import com.easycarpool.dao.impl.CarDetailsDaoImpl;
+import com.easycarpool.entity.DeviceInfo;
 import com.easycarpool.log.EasyCarpoolLogger;
 import com.easycarpool.log.IEasyCarpoolLogger;
 import com.google.android.gcm.server.Message;
@@ -15,16 +16,23 @@ public class NotificationSender {
 	private static final String GOOGLE_SERVER_KEY = "AIzaSyDA5dlLInMWVsJEUTIHV0u7maB82MCsZbU";
 	private static IEasyCarpoolLogger logger = EasyCarpoolLogger.getLogger();
 	private static String CLASS_NAME = NotificationSender.class.getName();
+	private static RegisterDevice registerDevice = new RegisterDevice();
 
-	public void sendNotification(String regId, String userMessage, String username){
+	public void sendNotification(String userMessage, String username){
 		final String MESSAGE_KEY = "message";	
 		Result result = null;
 		try {
-			Sender sender = new Sender(GOOGLE_SERVER_KEY);
-			Message message = new Message.Builder().timeToLive(30)
-					.delayWhileIdle(true).addData(MESSAGE_KEY, userMessage).build();
-			System.out.println("regId: " + regId);
-			result = sender.send(message, regId, 1);
+			DeviceInfo deviceInfo = registerDevice.getDeviceInfo(username);
+			String regId  = deviceInfo.getDeviceToken();
+			if(deviceInfo.getDeviceType().equalsIgnoreCase("android")){
+				Sender sender = new Sender(GOOGLE_SERVER_KEY);
+				Message message = new Message.Builder().timeToLive(30)
+						.delayWhileIdle(true).addData(MESSAGE_KEY, userMessage).build();
+				System.out.println("regId: " + regId);
+				result = sender.send(message, regId, 1);
+			}else{
+				
+			}
 			logger.log(Level.INFO, CLASS_NAME, "sendNotification", "Notification Pushed to "+username+" and pushStatus is : "+result.toString());
 		} catch (IOException ioe) {
 			logger.log(Level.ERROR, CLASS_NAME, "sendNotification", "Exception thrown while pushing notification to "+username+" and Exception : "+ioe);
--- EasyCarpool/src/com/easycarpool/notification/RegisterDevice.java
+++ EasyCarpool/src/com/easycarpool/notification/RegisterDevice.java
@@ -1,11 +1,60 @@
 package com.easycarpool.notification;
 
+import java.util.UUID;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Level;
+
+import com.easycarpool.entity.DeviceInfo;
+import com.easycarpool.log.EasyCarpoolLogger;
+import com.easycarpool.log.IEasyCarpoolLogger;
+import com.easycarpool.redis.RedisWrapper;
+import com.google.gson.Gson;
+
 public class RegisterDevice {
+	private static IEasyCarpoolLogger logger = EasyCarpoolLogger.getLogger();
+	private static String CLASS_NAME = RegisterDevice.class.getName();
+	private static final String mapName = "ec_deviceDetails";
+	private static RedisWrapper redisWrapper = new RedisWrapper();
 	
 	public String registerDeviceInfo(HttpServletRequest request){
-		return null;
+		DeviceInfo deviceInfo = new DeviceInfo();
+		try {
+			deviceInfo.setUsername(request.getParameter("username"));
+			deviceInfo.setDeviceToken(request.getParameter("deviceToken"));
+			deviceInfo.setAppId(UUID.fromString(request.getParameter("appId")));
+			deviceInfo.setDeviceType(request.getParameter("deviceType"));
+			redisWrapper.insert(deviceInfo.getUsername(), mapName, deviceInfo);
+		} catch (Exception e) {
+			logger.log(Level.ERROR, CLASS_NAME, "registerDeviceInfo", "Exception thrown while inserting value for deviceDetails for username : "+deviceInfo.getUsername()+" and Exception is : "+e.getMessage());
+			return "App could not be registered";
+		}
+		return "App registed successfully";
+	}
+	public DeviceInfo getDeviceInfo(String username){
+		DeviceInfo deviceInfo = new DeviceInfo();
+		try {
+			deviceInfo =(DeviceInfo) redisWrapper.get(username, mapName);
+		} catch (Exception e) {
+			logger.log(Level.ERROR, CLASS_NAME, "getDeviceInfo", "Exception thrown while fetching deviceDetails for username : "+username+" and Exception is : "+e.getMessage());
+		}
+		return deviceInfo;
+	}
+	public String removeDeviceInfo(HttpServletRequest request){
+		String username = null;
+		String msg = null;
+		try {
+			username = request.getParameter("username");
+			long result = redisWrapper.remove(username, mapName);
+			if(result!=0){
+				msg = "Device Info removed for username : "+username;
+			}
+		} catch (Exception e) {
+			logger.log(Level.ERROR, CLASS_NAME, "getDeviceInfo", "Exception thrown while fetching deviceDetails for username : "+username+" and Exception is : "+e.getMessage());
+			msg = "Device Info could not be removed for username : "+username;
+		}
+		return msg;
 	}
 
 }
